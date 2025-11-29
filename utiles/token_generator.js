54a1cdd3811be76b31505cee05e82bf0f4a05e34
Sergio Daniel Galván <sdg@reportsystem.com.ar>
2025-11-29 20:21:09 -0300

    verifyToken

--- utiles/token_generator.js
+++ utiles/token_generator.js
@@ -5,6 +5,17 @@ const secretKey = process.env.JWT_CLAVE_TOKEN || 'U1tr@S3cr3t!';
 
 export const generateToken = ( payload ) => {
   const user = {id: payload.id, email: payload.email};
-  const token = jwt.sign( payload, secretKey, { expiresIn: process.env.JWT_TIEMPO_EXPIRACION || '1h' } );
-  return token;
+  const tokenID = jwt.sign( payload, secretKey, { expiresIn: process.env.JWT_TIEMPO_EXPIRACION || '1h' } );
+  return tokenID;
+}
+
+export const verifyToken = ( token ) => {
+  try {
+    const decoded = jwt.verify( token, secretKey );
+    return decoded;
+    } catch ( error ) {         
+    }
+    finally {
+    }
+    return null;
 }
