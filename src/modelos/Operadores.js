c0ac4fba43660b508f57f8c1c62bc60a7b5da226
Sergio Daniel Galván <sdg@reportsystem.com.ar>
2025-12-11 13:25:26 -0300

    Implementacion en FireStore de Operadores

--- src/modelos/Operadores.js
+++ src/modelos/Operadores.js
@@ -1,26 +1,59 @@
 import { db } from "./firebase.js";
 import { collection, getDocs, doc, getDoc, addDoc, updateDoc, deleteDoc } from "firebase/firestore";
   
+const operadoresCollection = collection(db, "operadores");
+const LIMITE_INTENTOS_FALLIDOS = 5;
+
 export const getAllOperadores = async () => {
     try {
+        const snapshot = await getDocs(operadoresCollection);
+        const operadores = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        if ( !productos ) {
+            return {"error": "Operadores no encontrados"};
+        }
         return operadores;
     } 
     catch ( error ) {
-        console.error(error);
+        console.error('Error al obtener operadores desde Firestore:', error);
+        throw error;
+    }   
+    finally {
+    }   
+};
+
+export const getAllOperadoresActivos = async () => {
+    try {
+        const snapshot = await getDocs(operadoresCollection);
+        const operadores = snapshot.docs
+            .map(doc => ({ id: doc.id, ...doc.data() }))
+            .filter( operador => operador.intentos <= LIMITE_INTENTOS_FALLIDOS );
+        if ( !productos ) {
+            return {"error": "Operadores no encontrados"};
+        }
+        return operadores;
+    } 
+    catch ( error ) {
+        console.error('Error al obtener operadores desde Firestore:', error);
+        throw error;
     }   
     finally {
     }   
 };
 
 export const getOperadorById = async ( id ) => {
-    try {   
+    try {
+        const snapshot = await getDocs(operadoresCollection);
+        const operadores = snapshot.docs 
+            .map(doc => ({ id: doc.id, ...doc.data() }))
+            .find(operador => operadores.id === id);
         if ( !operador ) {
             return { "error": "Operador no encontrado"};
         }
         return operador;
     }
     catch ( error ) {
-        console.error(error);
+        console.error('Error al obtener producto por ID desde Firestore:', error);  
+        throw error;
     }
     finally {
     }
@@ -28,11 +61,14 @@ export const getOperadorById = async ( id ) => {
 
 export const getOperadorByEmail = async ( email ) => {
     try {
-        
-        if ( !operador ) {
-            return { "error": "Operador no encontrado"};
+        const snapshot = await getDocs(operadoresCollection);
+        const operadores = snapshot.docs 
+            .map(doc => ({ id: doc.id, ...doc.data() }))
+            .filter(producto => producto.email.startsWith(email));
+        if ( !operadores ) {
+            return { "error": "Operadores no encontrados"};
         }
-        return operador;
+        return operadores;
     }
     catch ( error ) {
         console.error(error);
@@ -43,10 +79,15 @@ export const getOperadorByEmail = async ( email ) => {
 
 export const createOperador = async ( operadorData ) => {
     try {
-        if ( !result ) {
-            return { "error": "No se pudo crear el operador"};
-        }   
-        return { id: result.insertId, nombre, email, tipooperador };
+        const nuevoOperador = {
+            email: operadorData.email, 
+            password: hashString( operadorData.password ),
+            nombre: operadorData.nombre,
+            intentos: 0,
+            fechaultimologin: null
+        };
+        const docRef = await addDoc(operadoresCollection, nuevoOperador);
+        return { id: docRef.id, ...nuevoOperador };
     }
     catch ( error ) {
         console.error(error);
@@ -55,14 +96,23 @@ export const createOperador = async ( operadorData ) => {
     }
 };
 
-
 export const deleteOperadorByEmail = async ( email ) => {
     try {
-        if ( !result ) {
-            return { "error": "No se pudo eliminar el operador"};
+        const coleccion = await getDocs(operadoresCollection);
+        const operador = coleccion.docs 
+            .map(doc => ({ id: doc.id, ...doc.data() }))
+            .filter(producto => producto.email === email );
+        if ( !operador ) {
+            return { "error": "Operador no encontrado"};
         }
-        return result;
-    }
+        const operadorRef = doc(operadoresCollection, operador.id );
+        const snapshot = await getDoc(operadorRef);
+        if (!snapshot.exists()) {
+            return false;
+        }
+        await deleteDoc(operadorRef);
+            return result;
+        }
     catch ( error ) {
         console.error(error);
     }
@@ -72,13 +122,17 @@ export const deleteOperadorByEmail = async ( email ) => {
 
 export const deleteOperadorById = async ( id ) => {
     try {
-        if ( !result ) {
-            return { "error": "No se pudo eliminar el operador"};
+        const operadorRef = doc(operadoresCollection, id);
+        const snapshot = await getDoc(operadorRef);
+        if ( !snapshot.exists() ) {
+          return false;
         }
-        return result;
+        await deleteDoc(operadorRef);
+        return true;
     }
     catch ( error ) {
-        console.error(error);
+        console.error('Error al eliminar Operador en Firestore:', error);
+        throw error;
     }
     finally {
     }
@@ -86,13 +140,29 @@ export const deleteOperadorById = async ( id ) => {
 
 export const updateOperadorByEmail = async ( email, operadorData ) => {
     try {
+        // Validaciones previas
+        if ( !email || typeof email !== 'string' || email.trim() === '' ) {
+            throw new Error("email de operador inválido");
+        }
+        const coleccion = await getDocs(operadoresCollection);
+        const operador = coleccion.docs 
+            .map(doc => ({ id: doc.id, ...doc.data() }))
+            .filter(producto => producto.email === email );
+        if ( !operador ) {
+            return { "error": "Operador no encontrado"};
+        }
+        const operadorRef = doc(operadoresCollection, operador.id );
+        const snapshot = await getDoc(operadorRef);
+        // Actualizar operador
+        result = await updateDoc(operadorRef, operadorData );   
         if ( !result ) {
             return { "error": "No se pudo actualizar el operador"};
         }
         return result;
     }
     catch ( error ) {
-        console.error(error);
+        console.error('Error al actualizar operador en Firestore:', error);
+        throw error;
     }
     finally {
     }
@@ -100,6 +170,17 @@ export const updateOperadorByEmail = async ( email, operadorData ) => {
 
 export const updateOperadorById = async ( id, operadorData ) => {
     try {
+        // Validaciones previas
+        if (!id || typeof id !== 'string') {
+        throw new Error("ID de producto inválido");
+        }
+        const operadorRef = doc(operadoresCollection, id);  
+        const snapshot = await getDoc(operadorRef);
+        if ( !snapshot.exists() ) {
+            throw new Error(`Operador con ID ${id} no encontrado`);
+        }
+        result = await updateDoc(operadorRef, operadorData );   
+
         if ( !result ) {
             return { "error": "No se pudo actualizar el operador"};
         }   
@@ -110,4 +191,24 @@ export const updateOperadorById = async ( id, operadorData ) => {
     }
     finally {
     }   
+};
+
+export const resetIntentos = async ( id ) => {
+    try {
+        const operadorRef = doc(operadoresCollection, id);
+        const snapshot = await getDoc(operadorRef);
+        if ( !snapshot.exists() ) {
+          return false;
+        }
+        await updateDoc(operadorRef, {
+          intentos: 0
+        });
+        return true;
+    }
+    catch ( error ) {
+        console.error('Error al resetear intentos en Firestore:', error);
+        throw error;
+    }
+    finally {
+    }
 };
--- src/modelos/Productos.js
+++ src/modelos/Productos.js
@@ -4,11 +4,12 @@ import { collection, getDocs, doc, getDoc, addDoc, updateDoc, deleteDoc } from "
 const productosCollection = collection(db, "productos");
 
 export const getAllProductos = async () => {
-  console.log("getAllProducto called");
   try {
     const snapshot = await getDocs(productosCollection);
-    console.log("Snapshot obtenido:", snapshot);
-    const productos = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));  
+    const productos = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    if ( !productos ) {
+      return {"error": "Productos no encontrados"};
+    }
     return productos;
   } catch (error) {
     console.error('Error al obtener productos desde Firestore:', error);
@@ -24,8 +25,12 @@ export const getAllProductosWithStock = async () => {
     const productos = snapshot.docs
       .map(doc => ({ id: doc.id, ...doc.data() }))
       .filter(producto => producto.stock > 0);  
+    if ( !productos ) {
+      return {"error": "Productos no encontrados"};
+    }
     return productos;
-  } catch ( error ) {
+  }
+  catch ( error ) {
     console.error('Error al obtener productos con stock desde Firestore:', error);  
     throw error;
   } 
@@ -43,7 +48,8 @@ export const getProductoById = async (id) => {
       return {"error": "Producto no encontrado"};
     }
     return productos;
-  } catch ( error ) {
+  }
+  catch ( error ) {
     console.error('Error al obtener producto por ID desde Firestore:', error);  
     throw error;
   }   
@@ -127,14 +133,14 @@ export const updateStockById = async (id, stock) => {
 
 export const deleteProductoById = async ( id ) => {
   try {
-    const productRef = doc(productsCollection, id);
-    const snapshot = await getDoc(productRef);
+    const productoRef = doc(productsCollection, id);
+    const snapshot = await getDoc(productoRef);
 
     if (!snapshot.exists()) {
       return false;
     }
 
-    await deleteDoc(productRef);
+    await deleteDoc(productoRef);
     return true;
   } catch ( error ) {
     console.error('Error al eliminar producto en Firestore:', error);
@@ -207,7 +213,6 @@ export const updateProductoWithStock = async ( id, stock ) => {
 export const updateProductoWithPrecio = async ( id, precio ) => {  
   try {
     // Validaciones previas
-
     if (!id || typeof id !== 'string') {
       throw new Error("ID de producto inválido");
     }
@@ -240,226 +245,4 @@ export const updateProductoWithPrecio = async ( id, precio ) => {
   finally {
     // cierre
   } 
-}; 
-
-/*
-export const getAllProductosOld = async (categoria, stock) => {
-  try {
-    let sql = 'SELECT id, nombre, precio, stock, categorias FROM productos';
-    const db = await connection();
-    const [productos] = await db.execute(sql);
-    if ( !productos ) {
-      return { "error": "No se encontraron productos"};
-    }
-    return productos;
-  }
-  catch (error) {
-    console.error('Error al obtener productos:', error);
-    throw error;
-  }
-  finally { 
-  }
-}
-*/
-
-/*
-export const getAllProductosWithStockOld = async () => { 
-  try {
-    let sql = 'SELECT id, nombre, precio, stock, categorias FROM productos WHERE stock > 0';
-    const db = await connection();
-    const [productos] = await db.execute(sql);
-    if ( !productos ) {
-      return { "error": "No se encontraron productos"};
-    }
-    return productos;
-  }
-  catch ( error ) {
-    console.error('Error al obtener productos con stock:', error);  
-    throw error;
-  }
-  finally {
-  } 
-}
-*/
-
-/*
-export const getAllProductos = async (categoria, stock) => {
-  try {
-    let sql = 'SELECT * FROM productos WHERE 1=1';
-    const params = [];
-
-    // Filtrar por stock > 0 si stock es true
-    if ( stock ) {
-      sql += ' AND stock > 0';
-    }
-
-    // Filtrar por categoría si se proporciona
-    if (categoria) {
-      // Como categorías está almacenado como JSON en la base de datos
-      sql += ' AND JSON_CONTAINS(categorias, ?)';
-      params.push(`"${categoria}"`); // Necesita las comillas para el JSON
-    }
-
-    // Ejecutar la consulta
-    const productos = await query(sql, params);
-    //const productos = await connection(sql, params);
-    
-    // Parsear el campo categorías de string JSON a array
-    if ( !productos ) {
-      const productosParseados = productos.map(producto => ({
-        ...producto,
-        categorias: JSON.parse(producto.categorias)
-      }));
-      return productosParseados;
-    }
-    else {
-      return { "error": "No se encontraron productos"};
-    }
-
-  } catch (error) {
-    console.error('Error al obtener productos:', error);
-    throw error;
-  }
-  finally {
-  }
 };
-*/
-
-/*
-export const getProductoByIdOld = async (id) => {
-  try {
-    let sql = 'SELECT id, nombre, precio, stock, categorias FROM productos WHERE id = ?';
-    const db = await connection();
-    const [productos] = await db.execute(sql, [id] );
-    if ( !productos ) {
-      return {"error": "Producto no encontrado"};
-    }
-    return productos;
-  } catch ( error ) {
-    console.error(error);
-  }
-  finally {
-  }
-};
-*/
-
-/*
-export const getProductoByNombreOld = async ( nombre ) => {
-  try {
-    let sql = 'SELECT id, nombre, precio, stock, categorias FROM productos WHERE nombre LIKE ?%';
-    const db = await connection();
-    const [producto] = await db.execute(sql, [nombre] );
-    if ( !productos ) {
-      return { "error": "No se encontraron productos"};
-    }
-    return producto;
-  } catch ( error ) {
-    console.error(error);
-  }
-  finally {
-  }
-};
-
-*/
-
-/*
-export const updateStockByIdOld = async ( id, stock ) => {
-  try {
-    let sql = 'UPDATE productos SET stock = ? WHERE id = ?';
-    const db = await connection();
-    const [result] = await db.execute(sql, [stock, id] );
-    if ( !productos ) {
-      return { "error": "No se encontró producto"};
-    }
-    return result;
-  } catch ( error ) {
-    console.error(error);
-  }
-  finally {
-  }
-}
-*/
-
-/*
-export const deleteProductoByIdOld = async ( id ) => {
-  try {
-    let sql = 'DELETE FROM productos WHERE id = ?';
-    const db = await connection();
-    const [result] = await db.execute(sql, [id] );
-    if ( !productos ) {
-      return { "error": "No se encontró producto"};
-    }
-    return result;
-  } catch ( error ) {
-    console.error(error);
-  }
-  finally {
-  }
-} 
-*/
-
-/*
-export const createProductoOld = async ( nombre, precio, categorias, stock ) => {
-  try {
-    let sql = 'INSERT INTO productos ( nombre, precio, categorias, stock ) VALUES ( ?, ?, ?, ? )';
-    const db = await connection();
-    const [producto] = await db.execute(sql, [ nombre, precio, JSON.stringify(categorias), stock ] ); 
-    return producto;
-  } catch (error) {
-    console.error(error);
-  }
-  finally {
-  }
-
-};
-*/
-
-/*
-export const getAllProductosByCategoria = async ( categoria ) => {  
-  try {
-    let sql = 'SELECT id, nombre, precio, stock, categorias FROM productos WHERE JSON_CONTAINS(categorias, ?)';
-    const db = await connection();
-    const [productos] = await db.execute(sql, [ `"${categoria}"` ] ); 
-    if ( !productos ) {
-      return { "error": "No se encontraron productos"};
-    }
-    return productos;
-  } catch ( error ) {
-    console.error(error);
-  } 
-  finally {
-  }
-};
-
-export const updateAllProductosWithStock = async ( id, stock ) => {
-  try {
-    let sql = 'UPDATE productos SET stock = ? WHERE id = ?';  
-    const db = await connection();
-    const [result] = await db.execute(sql, [ stock, id ] );
-    if ( !result ) {
-      return { "error": "No se encontró producto"};
-    } 
-    return result;
-  } catch ( error ) {
-    console.error(error);
-  }
-  finally {
-  }
-};
-
-export const updateAllProductosWithPrecio = async ( id, precio ) => {
-  try {
-    let sql = 'UPDATE productos SET precio = ? WHERE id = ?';
-    const db = await connection();
-    const [result] = await db.execute(sql, [ precio, id ] );
-    if ( !result ) {
-      return { "error": "No se encontró producto"};
-    }
-    return result;
-  } catch ( error ) {
-    console.error(error);
-  }
-  finally {
-  }
-};
-*/
